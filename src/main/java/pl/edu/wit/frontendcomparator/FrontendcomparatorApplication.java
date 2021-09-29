f0e42a2dbfc703c0350e78a2f3b6beb9abdff87a
Arthurgt <arturbabkiewicz@gmail.com>
2021-09-29 21:02:12 +0200

    Initialized project

---
+++ src/main/java/pl/edu/wit/frontendcomparator/FrontendcomparatorApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.wit.frontendcomparator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendcomparatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontendcomparatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/edu/wit/frontendcomparator/FrontendcomparatorApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.wit.frontendcomparator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontendcomparatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
